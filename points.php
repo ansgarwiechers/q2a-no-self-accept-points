a1e570129e2fc08b811627490874a4bac0007c39
Ansgar Wiechers <ansgar.wiechers@planetcobalt.net>
2020-09-24 23:47:12 +0200

    use null-safe comparison operator

--- points.php
+++ points.php
@@ -14,7 +14,7 @@ function qa_db_points_calculations() {
 	$points_calculations = qa_db_points_calculations_base();
 
 	if (array_key_exists('aselects', $points_calculations)) {
-		$points_calculations['aselects']['formula'] = "COUNT(*) AS aselects FROM ^posts AS userid_src JOIN ^posts AS answers ON userid_src.selchildid=answers.postid WHERE userid_src.userid~ AND userid_src.type='Q' AND (answers.userid <> userid_src.userid)";
+		$points_calculations['aselects']['formula'] = "COUNT(*) AS aselects FROM ^posts AS userid_src JOIN ^posts AS answers ON userid_src.selchildid=answers.postid WHERE userid_src.userid~ AND userid_src.type='Q' AND (answers.userid <=> userid_src.userid)";
 	} else {
 		// if key 'aselects' is missing: throw a warning, but continue regardless
 		trigger_error("Missing key 'aselects' in points calculations array", E_USER_WARNING);
